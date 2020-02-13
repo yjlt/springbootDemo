20bb6f662911d3f042293bdf54a4ca42a9e00915
Zhang Mohan <zmh1992819@163.com>
2020-02-13 16:28:05 +0800

    third commit

---
+++ src/test/java/com/yjlt/userinfo/UserinfoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.yjlt.userinfo;
+
+/**
+ * @program: userinfo
+ * @package: com.yjlt.userinfo
+ * @class: UserinfoApplicationTest
+ * @description
+ * @author: yjlt
+ * @date: 2020/2/13 16:27
+ * @Version 1.0
+ **/
+public class UserinfoApplicationTest {
+}
